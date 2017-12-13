e6cf7fba84484f0588b2d5f2ae0024580b45abaa
Bryan Cesar Aguilar Perez <Bryan@MacBook-Pro-de-Bryan.local>
2017-12-13 16:49:24 -0500

    commit inicial

---
+++ src/test/java/com/restaurant/restaurantservice/RestaurantserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restaurant.restaurantservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
